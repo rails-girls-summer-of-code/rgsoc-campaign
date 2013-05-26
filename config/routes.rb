2f00889fc17e6dc1a5f0a7dca6ae1fc2e8f03e16
Sven Fuchs <me@svenfuchs.com>
2013-05-26 13:27:22 +0200

    use sqlite in dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 require 'geo_ip'
 
 Rgsoc::Application.routes.draw do
-  match 'donate/:package', as: :new_donation, to: 'donations#new'
-  match 'subscriptions/:package', as: :new_subscription, to: 'orders#new', subscription: true
-  match 'geo_ip.json', to: GeoIP.new
-
   resources :donations do
     post 'checkout', on: :collection
     get 'confirm', on: :member
   end
 
   match '/donations.json', to: 'orders#index', as: :donors
-
-  # devise_for :users, controllers: { omniauth_callbacks: 'sessions' }
-  # as :user do
-  #   get 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_session
-  # end
+  match 'geo_ip.json', to: GeoIP.new
 end
