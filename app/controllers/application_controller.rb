80562408bee951dbf85c4cc19bca96e917e8bed1
Sven Fuchs <me@svenfuchs.com>
2013-06-02 17:43:15 +0200

    remove debug output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
     def require_http_auth
       authenticate_or_request_with_http_basic do |username, password|
-        p [username, password]
-        p [ENV['HTTP_AUTH_USER'], ENV['HTTP_AUTH_PASSWORD']]
         username == ENV['HTTP_AUTH_USER'] && password == ENV['HTTP_AUTH_PASSWORD']
       end
     end
