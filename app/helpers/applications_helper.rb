d21c25dede69b5b03cb6773502e8a5a2b833b9d7
Sven Fuchs <me@svenfuchs.com>
2013-06-09 17:35:48 +0200

    add project visibility to the list

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationsHelper
+  def format_application_project(application)
+    project = application.project_name
+    project = "#{project} (#{application.project_visibility})" if application.project_visibility
+    project
+  end
 end
