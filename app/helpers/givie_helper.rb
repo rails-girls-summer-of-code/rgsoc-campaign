b881bb330f8f839ee37e11d6d226bebbe3dd4f89
Duana Saskia <starkcoffee+apps@gmail.com>
2014-05-01 17:15:51 +0200

    Change givie_helper to encrypt params

--- app/helpers/givie_helper.rb
+++ app/helpers/givie_helper.rb
@@ -1,14 +1,17 @@
 module GivieHelper
-  
+
   def givie_url(donation)
-    "http://kitten-sandwich.herokuapp.com/pingback?" + 
-      "name=#{donation.name}" +
-      "&message=#{donation.comment}" + 
-      "&twitter=#{donation.twitter_handle}" + 
-      "&github=#{donation.github_handle}" +
-      "&gravatar=#{donation.gravatar_url}" +
-      "&homepage=#{donation.homepage}" +
-      "&uid=#{donation.id}"
+    crypt = ActiveSupport::MessageEncryptor.new(ENV['GIVIE_SECRET'])
+    data = crypt.encrypt_and_sign({
+      uid: donation.id.to_s,
+      name: donation.name,
+      message: donation.comment,
+      twitter: donation.twitter_handle,
+      github: donation.github_handle,
+      gravatar: donation.gravatar_url,
+      homepage: donation.homepage,
+    }.to_json)
+
+    "#{ENV['GIVIE_BASE_URL']}/pingback?data=" + data
   end
-  
 end
