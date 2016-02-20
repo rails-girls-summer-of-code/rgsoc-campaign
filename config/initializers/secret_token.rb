670f206e8e0580e1fc73d80b02f33973fb26e100
Markus Prinz <markus.prinz@nuclearsquid.com>
2016-02-20 17:08:57 +0100

    Update application config for Rails 4
    Changed/Updated configuration settings:
      * The way gems were loaded via bundler was updated
      * The `filter_parameters` config was moved to
        `config/initializers/filter_parameter_logging.rb`
      * The way SimpleForm configuration works has been updated
        for the current version of SimpleForm
    
    Removed configuration settings:
      * `config.encoded` now defaults to "utf-8"
      * JSON root element in ActiveRecord objects is now no longer included
        by default
    
    Added configuration settings:
      * Don't swallow errors in `after_rollback` or `after_commit` callbacks
        (`config.active_record.raise_in_transactional_callbacks`)
      * The `eager_loading` setting is configured appropriately for the
        various environments
      * Development: Raise an error if there are pending migrations on page
        load
      * Enable invalidation of assets via digests
      * Enable additional asset error checking at runtime in development
      * Raise error if there are missing translations in development
      * Use new Logger::Formatter for formatting logs in production
      * Default to debug log level in production
      * Randomize test order in test environment
      * `secret_key_base` in addition to `secret_token`

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Rgsoc::Application.config.secret_token = 'c7b987733369c7e0b9dca6938d2c0a688cafdf9f37b9254a99726bd203ae67e549269507e1325bd0beb17311dd43c80f7c80e9ff0a055aa1978ebda6cc60c3d0'
+Rgsoc::Application.config.secret_key_base = 'c7b987733369c7e0b9dca6938d2c0a688cafdf9f37b9254a99726bd203ae67e549269507e1325bd0beb17311dd43c80f7c80e9ff0a055aa1978ebda6cc60c3d0'
