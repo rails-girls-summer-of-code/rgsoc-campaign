d678e5df97856a4a74d29652171074f2bbf50585
Markus Prinz <markus.prinz@nuclearsquid.com>
2016-02-22 09:40:18 +0100

    Remove `app/mailer/student_mailer.rb`
    The class in it defines a method that isn't used anywhere, and it
    clashes with `app/mailers/student_mailer.rb`.

--- app/mailer/student_mailer.rb
+++
@@ -1,7 +0,0 @@
-class StudentMailer < ActionMailer::Base
-  default from: 'Rails Girls Summer of Code <summer-of-code-students@railsgirls.com>'
-
-  def acceptance_email(receiver)
-    mail to: receiver, subject: 'Rails Girls Summer of Code: student selection'
-  end
-end
